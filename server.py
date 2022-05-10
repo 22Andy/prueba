cd4dc526d97efe8a584b57141de7da1c4fff2d13
Andrea Ek <Andrea@Andreas-MacBook-Pro.local>
2022-05-10 12:22:23 -0500

    Revert "version 2.0 del proyecto"
    This reverts commit a5d918a994385a90e126e2bcfb727215725f45db.

--- server.py
+++ server.py
@@ -37,14 +37,8 @@ def val():
     tel=request.form['tel']
     ema=request.form['ema']
     contr=request.form['contr']
-    if fun.existeusuario(ema): # si existe el usuario
-        return render_template('registro.html')
-    else: # no existe el usuario
-        fun.insertuser(name,ap,tel,ema,contr)
-        return render_template('regisexi.html',name=name,ap=ap,tel=tel,ema=ema,contr=contr)
-        
-
-    
+    fun.insertuser(name,ap,tel,ema,contr)
+    return render_template('regisexi.html',name=name,ap=ap,tel=tel,ema=ema,contr=contr)
 
 if __name__== '__main__':
     app.run(port=PORT,debug=DEBUG)
